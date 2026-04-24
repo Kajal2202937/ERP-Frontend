f3fa4ca763ac4ce2842348138f40bc0ad4de5e49
Kajal2202937 <bbhumi908@gmail.com>
2026-04-24 21:10:18 +0530

    final fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import ThemeProvider from "./context/ThemeContext";
+import { ToastContainer } from "react-toastify";
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+      <AuthProvider>
+        <ToastContainer position="top-right" autoClose={3000} />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </AuthProvider>
+    </BrowserRouter>
 );
